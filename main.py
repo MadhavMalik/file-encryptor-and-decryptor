de66c64c3f53b5e1455ab4b3c72440e1fd433cc4
MadhavMalik <40149943+MadhavMalik@users.noreply.github.com>
2021-03-07 11:57:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from tkinter import filedialog
 from functools import partial
 
 global filename
+button_height = 2
+button_width = 25
 
 def browseFiles():
     browseFiles.filename = filedialog.askopenfilename(initialdir="/", title="Select a File",)
@@ -47,6 +49,7 @@ def decrypt_file(p_word):
 
 
 window = Tk()
+
 window.title('File Explorer')
 window.geometry("940x740")
 window.config(background="black")
@@ -56,21 +59,23 @@ passwd = StringVar()
 
 submit_para_en = partial(encrypt_file, passwd)
 submit_para_de = partial(decrypt_file, passwd)
-credit = Label(window,text = "Developed by Madhav Malik", bg="black",height=4,  fg = "white", font =("",15))
+
+credit = Label(window,text = "Developed by Madhav Malik", bg="black",height=2,  fg = "white", font =("",15))
 label_file_explorer = Label(window, text="File Name : ", width=100, height=2, fg="white", bg="black",font =("",20))
-pass_label = Label(window, text="Password for encryption/decryption : ", width=100, height=4, fg="white", bg="black",font =("",20))
+pass_label = Label(window, text="Password for encryption/decryption : ", width=100, height=2, fg="white", bg="black",font =("",20))
 temp_label = Label(window, text="", height=3, bg="black")
-button_explore = Button(window, text="Browse File", command=browseFiles, width=50, height=4, font =("",15))
+
+button_explore = Button(window, text="Browse File", command=browseFiles, width=button_width, height=button_height, font =("",15))
 
 password = Entry(window, textvariable=passwd,show="*")
 
-button_encrypt = Button(window, text="Encrypt", command=submit_para_en, width=50, height=4, font =("",15))
-button_decrypt = Button(window, text="Decrypt", command=submit_para_de, width=50, height=4, font =("",15))
+button_encrypt = Button(window, text="Encrypt", command=submit_para_en, width=button_width, height=button_height, font =("",15))
+button_decrypt = Button(window, text="Decrypt", command=submit_para_de, width=button_width, height=button_height, font =("",15))
 
 status_label = Label(window, text="", width=100, height=4, fg="white", bg="black",font =("",17))
+
 credit.pack()
 main_title.pack()
 label_file_explorer.pack()
 button_explore.pack()
-
 window.mainloop()
